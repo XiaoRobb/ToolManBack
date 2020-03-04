70df8ee845919c42eab230f0694405a6b98f858e
崎璇 王 <mightyactionx@163.com>
2020-03-04 11:08:39 +0800

    新建项目

---
+++ src/main/java/com/whu/toolman/ToolmanApplication.java
@@ -0,0 +1,13 @@
+package com.whu.toolman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolmanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whu/toolman/ToolmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whu.toolman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
