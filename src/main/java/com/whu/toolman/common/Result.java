4672b31d385c8c3fcaf9d1d182cd37a215f9d1a6
崎璇 王 <mightyactionx@163.com>
2020-03-29 18:02:21 +0800

    增加部分基础配置

---
+++ src/main/java/com/whu/toolman/common/Result.java
@@ -0,0 +1,10 @@
+package com.whu.toolman.common;
+
+/**
+ * @author ：qx.w
+ * @description：接口返回前端类
+ * @modified By：
+ * @since ：Created in 2020/3/4 14:11
+ */
+public class Result {
+}
---
+++ src/main/java/com/whu/toolman/controller/DemoController.java
@@ -0,0 +1,10 @@
+package com.whu.toolman.controller;
+
+/**
+ * @author ：qx.w
+ * @description：
+ * @modified By：
+ * @since ：Created in 2020/3/4 14:11
+ */
+public class DemoController {
+}
