ce2968c181bbe2feb548a4f48a769b86ea28b2ab
陈嘉 <1433119499@qq.com>
2020-05-13 12:31:49 +0800

    AudioController

--- src/test/java/com/whu/toolman/ToolmanApplicationTests.java
+++ src/test/java/com/whu/toolman/ToolmanApplicationTests.java
@@ -2,6 +2,7 @@ package com.whu.toolman;
 import com.whu.toolman.picHandle.PicHandle;
 import com.whu.toolman.picHandle.PicRenderingHandle;
 import com.whu.toolman.picHandle.xunfeiocr;
+import  com.whu.toolman.util.*;
 import  com.whu.toolman.util.AudioConvertUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,8 @@ class ToolmanApplicationTests {
     void contextLoads() throws Exception {
         //PicHandle.compressImage("D://b.jpg","D://c.jpg",500,0.9);
       // AudioConvertUtils.picAudioFromVideo("G:\\英雄时刻\\191367444\\英雄时刻_20200503-17点56分36s.avi","C:\\Users\\陈嘉\\Desktop\\g.wav");
-
+        //String s = JsonUtil.GetContent(":);
+        //System.out.print(s);
 
     }
 
